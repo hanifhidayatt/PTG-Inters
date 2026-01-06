ddf897471fa6fc7dae383a84e843c98e4506c221
Hanif Hidayat <hanhidayattt@gmail.com>
2026-01-06 13:51:49 +0800

    Setting the parent class

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person {
+    protected String name;
+    protected int age;
+    protected String gender;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Age: " + age + ", Gender: " + gender);
+    }
+}
